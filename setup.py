547bd6912afa0ef57f3c6e4bc57f2330347f6710
Nickolaj <nickolaj1177@gmail.com>
2015-12-19 23:44:09 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='cuepy',
     packages=['cuepy'],
-    version='0.1.0',
+    version='0.1.1',
     description='Python wrapper for the corsair utility engine',
     author='Nickolaj Jepsen',
     author_email='nickolaj1177@gmail.com',
-    url='https://github.com/fire-proof/cuepy',  # use the URL to the github repo
-    download_url='https://github.com/fire-proof/cuepy/tarball/0.1',  # I'll explain this in a second
-    keywords=['Corsair', 'CUE', 'Keyboard'],  # arbitrary keywords
+    url='https://github.com/fire-proof/cuepy',
+    download_url='https://github.com/fire-proof/cuepy/tarball/0.1.1',
+    keywords=['Corsair', 'CUE', 'Keyboard'],
     classifiers=[],
 )
